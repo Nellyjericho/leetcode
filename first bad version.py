12b2db05ad2cbde324fffa3adbb6dc1100fd8771
Nelly Jericho <nellyjericho55@gmail.com>
2026-07-23 06:40:24 +0300

    278 first bad version

---
+++ first bad version.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+    #runtime = 15ms memory = 12.40mb
